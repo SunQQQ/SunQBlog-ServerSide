0de556b4cb20be2a000ceffe7f6db9b1b6145923
sunquan <1585437938@qq.com>
2021-08-12 16:39:11 +0800

    新增删除时间轴接口

--- index.js
+++ index.js
@@ -389,6 +389,18 @@ App.post('/TimeLineRead/:accesstype', function (Request, Response) {
     });
   });
 });
+// 删除时间轴
+App.post('/TimeLineDelete/:accesstype', function (Request, Response) {
+  DealPara(Request, Response, function (Para) {
+    var Object = {};
+    Object._id = ObjectId(Para._id);
+
+    Monge.Mongo('TimeLine', 'Delete', Object, function () {
+      var Json = {status: '0', data: '时间轴删除成功'};
+      Response.json(Json);
+    });
+  });
+});
 
 // 获取心声
 App.post('/HeartfeltRead/:accesstype', function (Request, Response) {
