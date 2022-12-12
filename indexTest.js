c79b3b871ec37ce62772cda00251eafccea3be71
sunquan <1585437938@qq.com>
2022-12-12 17:34:38 +0800

    改造入口index文件

--- indexTest.js
+++ indexTest.js
@@ -2,14 +2,13 @@
  * author: sunquan
  * 本文件实现孙权的博客的所有接口
  */
- var express = require('express');
- var os = require('os');
- 
- var FS = require('fs');
- var App = express();
- var cors = require('cors');
- var BodyParse = require('body-parser');
- 
+let express = require('express');
+let os = require('os');
+let FS = require('fs');
+let App = express();
+let cors = require('cors');
+let BodyParse = require('body-parser');
+// 各个页面使用的接口
 let blogIndex = require("./page/blogIndex.js")(App);
 let comment = require("./page/comment.js")(App);
 let heartFelt = require("./page/heartFelt.js")(App);
@@ -18,10 +17,6 @@ let timeLine = require("./page/timeLine.js")(App);
 let user = require("./page/user.js")(App);
 let visitCount = require("./page/visitCount.js")(App);
 
-App.use(cors());
-App.use(BodyParse.json());
-App.use(BodyParse.urlencoded({ extended: true }));
-
 blogIndex();
 comment();
 heartFelt();
@@ -29,14 +24,16 @@ messageCreate();
 timeLine();
 user();
 visitCount();
- 
-var server = App.listen(8088, function () {
- 
-     var host = server.address().address
-     var port = server.address().port
- 
-     console.log("Node执行地址 http://%s:%s", host, port)
- 
- });
- // 静态资源路径
- App.use(express.static('Public'));
+
+App.use(cors());
+App.use(BodyParse.json());
+App.use(BodyParse.urlencoded({ extended: true }));
+
+let server = App.listen(8088, function () {
+    let host = server.address().address
+    let port = server.address().port
+
+    console.log("Node执行地址 http://%s:%s", host, port)
+});
+// 静态资源路径
+App.use(express.static('Public'));
