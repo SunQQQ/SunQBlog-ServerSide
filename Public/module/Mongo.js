5d5840df3a33ee5a6f2e10515c976615c4a611f3
sunquan <1585437938@qq.com>
2023-09-14 09:26:25 +0800

    开发两级留言接口

--- Public/module/Mongo.js
+++ Public/module/Mongo.js
@@ -13,55 +13,87 @@ var Url = "mongodb://localhost:27017/";
  * @constructor
  */
 
-function Mongo(Collection,Type, data, CallBack) {
-  MongoClient.connect(Url, {useUnifiedTopology: true},function (err, db) {
+function Mongo(Collection, Type, data, CallBack) {
+  MongoClient.connect(Url, { useUnifiedTopology: true }, function (err, db) {
     var DB = db.db("test");
     if (Type == 'Insert') {
       DB.collection(Collection).insertOne(data, function (err, res) {
-        if(err) throw err;
+        if (err) throw err;
         db.close();
         CallBack(res);
       });
     } else if (Type == 'Delete') {
-      DB.collection(Collection).deleteOne(data,function (err,res) {
-        if(err) throw err;
+      DB.collection(Collection).deleteOne(data, function (err, res) {
+        if (err) throw err;
         db.close();
         CallBack(res);
       });
     } else if (Type == 'Read') {
       DB.collection(Collection).find(data).toArray(function (err, res) {
-        if(err) throw err;
+        if (err) throw err;
         db.close();
         CallBack(res);
       });
-    } else if(Type == 'Update'){
-      DB.collection(Collection).updateOne(data[0],data[1],function (err,res) {
-        if(err) throw  err;
+    } else if (Type == 'Update') {
+      DB.collection(Collection).updateOne(data[0], data[1], function (err, res) {
+        if (err) throw err;
         db.close();
         CallBack(res);
       })
       // 获取列表数量
-    }else if(Type == 'GetNum'){
+    } else if (Type == 'GetNum') {
       DB.collection(Collection).find(data).toArray(function (err, res) {
-        if(err) throw err;
+        if (err) throw err;
         db.close();
         CallBack(res.length);
       });
       // 获取排序后的列表
-    }else if(Type == 'ReadByOrder'){
-      if(data[2]){
+    } else if (Type == 'ReadByOrder') {
+      if (data[2]) {
         DB.collection(Collection).find(data[0]).sort(data[1]).skip(data[2].Skip).limit(data[2].Limit).toArray(function (err, res) {
-          if(err) throw err;
+          if (err) throw err;
           db.close();
           CallBack(res);
         });
-      }else {
+      } else {
         DB.collection(Collection).find(data[0]).sort(data[1]).toArray(function (err, res) {
-          if(err) throw err;
+          if (err) throw err;
           db.close();
           CallBack(res);
         });
       }
+    } else if (Type == 'twoLeavlList') {
+      // 查询二级评论数据
+      DB.collection(Collection).aggregate([
+        {
+          $sort: {
+            _id: -1
+          }
+        },
+        {
+          $lookup: { // 将评论文档与其父级文档关联
+            from: Collection,
+            localField: "_id",
+            foreignField: "parentId",
+            as: "son"
+          }
+        },
+        {
+          $match: { // 筛选出只有父级文档的评论文档，排除顶级评论档
+            parentId: { $in: ["", null] }
+          }
+        },
+        {
+          $skip: data.Skip
+        }, // 跳过(page-1)*page_size条文档数
+        {
+          $limit: data.Limit// 限制返回的文档数为page_size
+        }
+      ]).toArray(function (err, res) {
+        if (err) throw err;
+        db.close();
+        CallBack(res);
+      });
     }
   });
 }
