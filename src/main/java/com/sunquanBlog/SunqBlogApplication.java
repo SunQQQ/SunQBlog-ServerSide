2eae8b062e80ddcf28072e07e566f499f31a01c4
sunquan <1585437938@qq.com>
2025-01-06 14:18:20 +0800

    恢复2.0主应用

---
+++ src/main/java/com/sunquanBlog/SunqBlogApplication.java
@@ -0,0 +1,15 @@
+package com.sunquanBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunqBlogApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SunqBlogApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
