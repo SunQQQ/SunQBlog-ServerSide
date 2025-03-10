5e09e0dc09024b05d397509e62ba75914aad97dc
sunquan <1585437938@qq.com>
2025-03-10 21:55:45 +0800

    配置代理

---
+++ src/main/java/com/sunquanBlog/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.sunquanBlog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://39.107.193.226")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 允许的请求方法
+                .allowedHeaders("*")  // 允许的请求头
+                .allowCredentials(true)  // 允许携带凭证（如 Cookie）
+                .maxAge(3600);  // 预检请求的缓存时间
+    }
+}
