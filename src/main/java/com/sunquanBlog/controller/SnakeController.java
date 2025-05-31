ad5d103c6f7b6026314b4bbbea0fbb55842576d1
sunquan <1585437938@qq.com>
2025-05-31 21:59:04 +0800

    贪吃蛇接口添加权限管理

--- src/main/java/com/sunquanBlog/controller/SnakeController.java
+++ src/main/java/com/sunquanBlog/controller/SnakeController.java
@@ -5,7 +5,9 @@ import com.sunquanBlog.model.Snake;
 import com.sunquanBlog.model.TimeLine;
 import com.sunquanBlog.service.LogService;
 import com.sunquanBlog.service.SnakeService;
+import com.sunquanBlog.service.SysLoginService;
 import com.sunquanBlog.service.TimeLineService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +24,8 @@ public class SnakeController {
     private SnakeService snakeService;
     @Autowired
     private LogService logService;
+    @Autowired
+    private SysLoginService sysLoginService;
 
     @PostMapping("/getSnakeScoreList")
     public ApiResponse getSnakeScoreList(@RequestBody Map<String,Object> requestBody) {
@@ -62,7 +66,15 @@ public class SnakeController {
     }
 
     @PostMapping("/scoreMulDelete")
-    public ApiResponse scoreMulDelete(@RequestBody Map<String, Object> requestBody) {
+    public ApiResponse scoreMulDelete(@RequestBody Map<String, Object> requestBody, HttpServletRequest request) {
+        Claims claims = (Claims) request.getAttribute("claims");
+        Integer userId = claims.get("id", Integer.class);
+        String role = sysLoginService.getUserById(userId).getRole();
+
+        if(!role.equals("master")) {
+            return ApiResponse.error(500,"权限不足");
+        }
+
         List ids = (List) requestBody.get("ids");
         Integer response = snakeService.scoreMulDelete(ids);
         if (response > 0) {
