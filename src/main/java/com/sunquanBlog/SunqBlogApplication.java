ef93c6f86de6ea0c410d9c13e2e1575c30320553
sunquan <1585437938@qq.com>
2025-07-02 22:20:24 +0800

    精细化管理日志

--- src/main/java/com/sunquanBlog/SunqBlogApplication.java
+++ src/main/java/com/sunquanBlog/SunqBlogApplication.java
@@ -1,15 +1,14 @@
 package com.sunquanBlog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SunqBlogApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(SunqBlogApplication.class, args);
-        System.out.println("hello world");
+        log.info("项目已启动成功");
     }
-
 }
